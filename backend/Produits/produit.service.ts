0d1ad5338a4aa6cbd084cd81634a1ef678586c60
Pakku73 <12monstroplantes@gmail.com>
2025-03-04 10:55:02 +0100

    catégories probleme requete

--- backend/Produits/produit.service.ts
+++ backend/Produits/produit.service.ts
@@ -1,35 +1,48 @@
-import db from '../config/db';
-import { Produit } from './produit.interfaces';
+import db from '../config/db'
+import { Produit } from './produit.interfaces'
 
 export class ProduitService {
   static async getAll(): Promise<Produit[]> {
-    const [rows] = await db.query('SELECT * FROM t_produit');
-    return rows as Produit[];
+    const [rows] = await db.query('SELECT * FROM t_produit')
+    return rows as Produit[]
+  }
+
+  static async getByForme(forme: string): Promise<Produit[] | null> {
+    const [rows] = await db.query('SELECT * FROM t_produit WHERE forme = ?it', [forme])
+    return (rows as any[]).map(rows => ({
+      id_t_produit: rows.id_t_produit,
+      nom_produit: rows.nom_produit,
+      description: rows.description,
+      forme: rows.forme,
+      dosage: rows.dosage,
+      prix: rows.prix,
+      laboratoire_fabriquant: rows.laboratoire_fabriquant,
+    }))
   }
 
   static async getById(id: number): Promise<Produit | null> {
-    const [rows] : any = await db.query('SELECT * FROM t_produit WHERE id_t_produit = ?', [id]);
-    return rows[0] as Produit || null;
+    const [rows]: any = await db.query('SELECT * FROM t_produit WHERE id_t_produit = ?', [id])
+    return (rows as Produit) || null
   }
 
   static async create(produit: Produit): Promise<number> {
-    const { nom_produit, description, forme, dosage, prix, laboratoire_fabriquant } = produit;
+    const { nom_produit, description, forme, dosage, prix, laboratoire_fabriquant } = produit
     const [result]: any = await db.query(
       'INSERT INTO t_produit (nom_produit, description, forme, dosage, prix, laboratoire_fabriquant) VALUES (?, ?, ?, ?, ?, ?)',
-      [nom_produit, description, forme, dosage, prix, laboratoire_fabriquant]
-    );
-    return result.insertId;
+      [nom_produit, description, forme, dosage, prix, laboratoire_fabriquant],
+    )
+    return result.insertId
   }
 
   static async update(id: number, produit: Produit): Promise<void> {
-    const { nom_produit, description, forme, dosage, prix, laboratoire_fabriquant } = produit;
+    const { nom_produit, description, forme, dosage, prix, laboratoire_fabriquant } = produit
     await db.query(
       'UPDATE t_produit SET nom_produit = ?, description = ?, forme = ?, dosage = ?, prix = ?, laboratoire_fabriquant = ? WHERE id_t_produit = ?',
-      [nom_produit, description, forme, dosage, prix, laboratoire_fabriquant, id]
-    );
+      [nom_produit, description, forme, dosage, prix, laboratoire_fabriquant, id],
+    )
   }
 
   static async delete(id: number): Promise<void> {
-    await db.query('DELETE FROM t_produit WHERE id_t_produit = ?', [id]);
+    await db.query('DELETE FROM t_produit WHERE id_t_produit = ?', [id])
   }
 }
--- frontend/src/page/NosProduits.tsx
+++ frontend/src/page/NosProduits.tsx
@@ -1,63 +1,85 @@
-import { useEffect, useState } from 'react';
-import Card from '../component/Card';
-import { Produit } from '../types'; // Importez le type Produit
+import { useEffect, useState } from 'react'
+import Card from '../component/Card'
+import { Produit } from '../types' // Importez le type Produit
 
 function NosProduits() {
-  const [produits, setProduits] = useState<Produit[]>([]);
-  const [messageSucces, setMessageSucces] = useState<string | null>(null); // État pour le message de succès
+  const [produits, setProduits] = useState<Produit[]>([])
+  const [messageSucces, setMessageSucces] = useState<string | null>(null) // État pour le message de succès
+  const [selectedForme, setSelectedForme] = useState<string>('tout') // Etat pour le filtre
 
   useEffect(() => {
     const getProduits = async () => {
       try {
-        const token = localStorage.getItem('authToken');
+        const token = localStorage.getItem('authToken')
 
-        const response = await fetch('http://localhost:3000/api/produits', {
+        const url =
+          selectedForme === 'tout'
+            ? 'http://localhost:3000/api/produits'
+            : 'http://localhost:3000/api/produits?forme=${selectedForme}'
+
+        const response = await fetch(url, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
           },
-        });
+        })
 
         if (!response.ok) {
-          throw new Error('Problème avec la requête');
+          throw new Error('Problème avec la requête')
         }
 
-        const data: Produit[] = await response.json();
-        setProduits(data);
+        const data: Produit[] = await response.json()
+        setProduits(data)
       } catch (error) {
-        console.error('Erreur de requête:', error);
+        console.error('Erreur de requête:', error)
       }
-    };
+    }
 
-    getProduits();
-  }, []);
+    getProduits()
+  }, [selectedForme])
 
- function addToPanier(produit: Produit){
-    const storedPanier = localStorage.getItem('panier');
-    const panier: Produit[] = storedPanier ? JSON.parse(storedPanier) : [];
-    panier.push(produit);
-    localStorage.setItem('panier', JSON.stringify(panier));
+  function addToPanier(produit: Produit) {
+    const storedPanier = localStorage.getItem('panier')
+    const panier: Produit[] = storedPanier ? JSON.parse(storedPanier) : []
+    panier.push(produit)
+    localStorage.setItem('panier', JSON.stringify(panier))
 
     // Afficher un message de succès
-    setMessageSucces(`${produit.nom_produit} a été ajouté au panier !`);
-    setTimeout(() => setMessageSucces(null), 3000); // Effacer le message après 3 secondes
-  };
+    setMessageSucces(`${produit.nom_produit} a été ajouté au panier !`)
+    setTimeout(() => setMessageSucces(null), 3000) // Effacer le message après 3 secondes
+  }
 
   return (
     <>
       {messageSucces && (
-        <div style={{ backgroundColor: 'green', color: 'white', padding: '10px', textAlign: 'center' }}>
+        <div
+          style={{ backgroundColor: 'green', color: 'white', padding: '10px', textAlign: 'center' }}
+        >
           {messageSucces}
         </div>
       )}
+
+      <div className="flex justify-center mb-3">
+        <select
+          value={selectedForme}
+          onChange={e => setSelectedForme(e.target.value)}
+          className="p-2 border border-grey-300 rounded-md"
+        >
+          <option value={'tout'}>Tout</option>
+          <option value={'dermique'}>dermique</option>
+          <option value={'injectable'}>injectable</option>
+          <option value={'médicamenteuse'}>médicamenteuse</option>
+        </select>
+      </div>
+
       {produits.length === 0 ? (
         <p>Requête produit...</p>
       ) : (
         <Card produits={produits} addToPanier={addToPanier} />
       )}
     </>
-  );
+  )
 }
 
-export default NosProduits;
+export default NosProduits
